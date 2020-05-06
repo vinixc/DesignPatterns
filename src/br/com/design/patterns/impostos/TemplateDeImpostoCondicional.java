24cb8b35ca452c461cf6844e33794ad2a0dfd0d6
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-05-06 09:24:16 -0300

    filha n pode sobreescrever o método da classe pai

--- src/br/com/design/patterns/impostos/TemplateDeImpostoCondicional.java
+++ src/br/com/design/patterns/impostos/TemplateDeImpostoCondicional.java
@@ -5,7 +5,7 @@ import br.com.design.patterns.model.Orcamento;
 public abstract class TemplateDeImpostoCondicional implements Imposto{
 
 	@Override
-	public double calcula(Orcamento orcamento) {
+	public final double calcula(Orcamento orcamento) {
 		if(deveUsarMaximaTaxacao(orcamento)) {
 			return maximaTaxacao(orcamento);
 		}
